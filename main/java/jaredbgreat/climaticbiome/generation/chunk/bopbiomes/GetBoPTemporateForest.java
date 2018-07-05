565cc93cc3bc7bc26cb8e4f929ea6644e802eff0
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2018-07-04 20:39:26 -0400

    More on BoP integration

--- main/java/jaredbgreat/climaticbiome/generation/chunk/bopbiomes/GetBoPTemporateForest.java
+++ main/java/jaredbgreat/climaticbiome/generation/chunk/bopbiomes/GetBoPTemporateForest.java
@@ -4,11 +4,28 @@ import jaredbgreat.climaticbiome.generation.chunk.ChunkTile;
 import jaredbgreat.climaticbiome.generation.chunk.biomes.IBiomeSpecifier;
 
 public class GetBoPTemporateForest implements IBiomeSpecifier {
+	private static int cherries, orchard, redwood, woodland;
 
 	@Override
 	public int getBiome(ChunkTile tile) {
-		// TODO Auto-generated method stub
-		return 0;
+		if((tile.getBiomeSeed() & 1) == 0) {
+			return woodland;
+		}
+		switch(tile.getBiomeSeed() % 3) {
+			case 0:
+				return cherries;
+			case 1:
+				return orchard;
+			case 2:
+				return redwood;
+			default:
+				return woodland;
+		}
+	}
+	
+	
+	public static void init() {
+		
 	}
 
 }
