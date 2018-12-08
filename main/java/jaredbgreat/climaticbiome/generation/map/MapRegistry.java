a6e483c1d06c3a2a24fc51f11ab9fdc59630f939
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2018-12-08 12:10:32 -0500

    The mod now save and reads its climate map

--- main/java/jaredbgreat/climaticbiome/generation/map/MapRegistry.java
+++ main/java/jaredbgreat/climaticbiome/generation/map/MapRegistry.java
@@ -3,13 +3,15 @@ package jaredbgreat.climaticbiome.generation.map;
 import static jaredbgreat.climaticbiome.util.ModMath.modRight;
 import jaredbgreat.climaticbiome.biomes.SubBiomeRegistry;
 import jaredbgreat.climaticbiome.generation.cache.Cache;
+import jaredbgreat.climaticbiome.generation.cache.Coords;
 import jaredbgreat.climaticbiome.generation.generator.BiomeBasin;
 import jaredbgreat.climaticbiome.generation.generator.MapMaker;
 import jaredbgreat.climaticbiome.util.SpatialNoise;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
@@ -74,6 +76,9 @@ public class MapRegistry {
 	
 	
 	private File getSaveFile(int x, int z) {
+		if(savedir == null) {
+			findSaveDir();			
+		}
 		return new File(savedir.toString() + File.separator + "X" + x + "Z" + z + ".cbmap");
 	}
 	
@@ -97,7 +102,7 @@ public class MapRegistry {
 		RegionMap out = data.get(x, z);
 		if(out == null) {
 			out = new RegionMap(x, z);
-			initializeMap(out);
+			readMap(out);
 			data.add(out);
 		}
 		return out;
@@ -159,13 +164,64 @@ public class MapRegistry {
 	 */
 	private void initializeMap(RegionMap map) {		
 		maker.generate(map);
-		if(savedir == null) {
-			findSaveDir();			
+		writeMap(map);
+	}
+	
+	
+	private void readMap(RegionMap map) {
+		Coords coords = map.getCoords();
+		int x = coords.getX();
+		int z = coords.getZ();
+		File file = getSaveFile(x, z);
+		short[] data = map.getData();
+		if(file.exists()) {
+			try {
+				
+				System.out.println();
+				System.out.println("****************************");
+				System.out.println("Reading map " + x + ", " + z);
+				System.out.println("****************************");
+				System.out.println();
+				
+				FileInputStream fs = new FileInputStream(file);
+				for(int i = 0; i < 65536; i++) {
+						data[i] = (short)fs.read();
+						data[i] |= (fs.read() << 8);
+					}
+				fs.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+		} else {
+			initializeMap(map);
 		}
-		File out = getSaveFile(map.getCoords().getX(), map.getCoords().getZ());
+	}
+	
+	
+	private void writeMap(RegionMap map) {
+		Coords coords = map.getCoords();
+		int x = coords.getX();
+		int z = coords.getZ();
+		File file = getSaveFile(x, z);
+		short[] data = map.getData();
+		
+		System.out.println();
+		System.out.println("****************************");
+		System.out.println("Writing map " + x + ", " + z);
+		System.out.println("****************************");
+		System.out.println();
+		
 		try {
-			BufferedWriter os = new BufferedWriter(new FileWriter(out));
-			os.close();
+			FileOutputStream fs = new FileOutputStream(file);
+			for(int i = 0; i < 65536; i++) {
+					fs.write(data[i] & 0xff);
+					fs.write((data[i] & 0xff00) >> 8);
+				}
+			fs.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- main/java/jaredbgreat/climaticbiome/generation/map/RegionMap.java
+++ main/java/jaredbgreat/climaticbiome/generation/map/RegionMap.java
@@ -237,6 +237,9 @@ public class RegionMap extends AbstractCachable {
     }
     
     
+    short[] getData() {
+    	return data;
+    }
     
     
     
