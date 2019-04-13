4a55683071e122cf217d8183198f505d05e1cc9a
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2019-04-13 07:19:03 -0400

    Can't make pine cone lang file work because reality is not self consistent

--- dt-compat/main/java/jaredbgreat/climaticbiome/compat/dynamictrees/Info.java
+++ dt-compat/main/java/jaredbgreat/climaticbiome/compat/dynamictrees/Info.java
@@ -5,9 +5,9 @@ public class Info {
 	public static final String ID = "climaticbiomesdt";
 	public static final String NAME = "Climatic Biomes Dynamic Trees Compat";
 	public static final String DIR = "ClimaticBiomes";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 	public static final String MINECRAFT = "1.12.2";
-	public static final String CHANNEL =  "climaticbiomedt";
+	public static final String CHANNEL =  "climaticbiomesdt";
 	
 	// Dependencies, for simplicity elsewhere
 	public static final String DEPSTR = "required-after:climaticbiomesjbg; required-after:dynamictrees";
